9439a8de0b413475870d9acb9651b5916f95ad39
ingr.in <aco@ingr.in>
2026-08-02 15:20:36 +0530

    Update sinu.js

--- xiku/ilu/social/sinu.js
+++ xiku/ilu/social/sinu.js
@@ -1,5 +1,5 @@
 // api.sinu - Fetch and display social media links
-const Sinu= document.querySelector('sinu').dataset('prem');
+const Sinu= document.querySelector('sinu').getAttribute('prem');
 async function loadSocialLinks() {
   try {
     const response = await fetch('Sinu');
