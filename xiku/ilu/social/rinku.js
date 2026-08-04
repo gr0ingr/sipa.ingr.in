729c7cba33b307285cdd6f0a9a9fb5d232057601
ingr.in <aco@ingr.in>
2026-08-04 21:01:22 +0530

    Create rinku.js

---
+++ xiku/ilu/social/rinku.js
@@ -0,0 +1,36 @@
+// config.js - Configuration constants
+export const SOCIAL_CONFIG = {
+  urlPatterns: {
+    youtube: (u) => `https://youtube.com/@${u}`,
+    instagram: (u) => `https://instagram.com/${u}`,
+    facebook: (u) => `https://facebook.com/${u}`,
+    facebookPage: (u) => `https://www.facebook.com/profile.php?id=${u}`,
+    twitter: (u) => `https://twitter.com/${u}`,
+    linkedin: (u) => `https://linkedin.com/in/${u}`,
+    github: (u) => `https://github.com/${u}`
+  },
+  displayNames: {
+    youtube: 'YouTube',
+    instagram: 'Instagram',
+    facebook: 'Facebook',
+    twitter: 'Twitter',
+    linkedin: 'LinkedIn',
+    github: 'GitHub'
+  },
+  iconClass: {
+    youtube: "fa-brands fa-youtube",
+    instagram: "fa-brands fa-instagram",
+    facebook: "fa-brands fa-facebook",
+    twitter: "fa-brands fa-x-twitter",
+    linkedin: "fa-brands fa-linkedin",
+    github: "fa-brands fa-github"
+  },
+icons: {
+    youtube: '▶️',
+    instagram: '📷',
+    facebook: '👍',
+    twitter: '🐦',
+    linkedin: '💼',
+    github: '🐙'
+  }
+};
